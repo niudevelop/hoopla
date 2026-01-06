d0fef5b5e3b0dc34c910d100410a827be7983c6d
niudevelop <info@niudevelop.com>
2026-01-06 21:05:37 +0100

    generating embeddings

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,11 +3,16 @@
 import argparse
 
 from lib.keyword_search import (
+    BM25_K1,
+    BM25_B,
+    bm25_tf_command,
+    bm25_idf_command,
     search_command,
     build_command,
     tf_command,
     idf_command,
     tfidf_command,
+    bm25search_command,
 )
 
 
@@ -33,6 +38,33 @@ def main() -> None:
     tfidf_parser.add_argument("doc_id", type=int, help="Document ID")
     tfidf_parser.add_argument("term", type=str, help="Term")
 
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument(
+        "term", type=str, help="Term to get BM25 IDF score for"
+    )
+
+    bm25_tf_parser = subparsers.add_parser(
+        "bm25tf", help="Get BM25 TF score for a given document ID and term"
+    )
+    bm25_tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    bm25_tf_parser.add_argument("term", type=str, help="Term to get BM25 TF score for")
+    bm25_tf_parser.add_argument(
+        "k1", type=float, nargs="?", default=BM25_K1, help="Tunable BM25 K1 parameter"
+    )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Normalization Strength"
+    )
+
+    bm25search_parser = subparsers.add_parser(
+        "bm25search", help="Search movies using full BM25 scoring"
+    )
+    bm25search_parser.add_argument("query", type=str, help="Search query")
+    bm25search_parser.add_argument(
+        "--limit", type=int, default=5, help="Maximum number of results to return"
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -53,6 +85,21 @@ def main() -> None:
             print(
                 f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}"
             )
+        case "bm25idf":
+            bm25idf = bm25_idf_command(args.term)
+            print(f"BM25 IDF score of '{args.term}': {bm25idf:.2f}")
+        case "bm25tf":
+            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1, args.b)
+            print(
+                f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25tf:.2f}"
+            )
+        case "bm25search":
+            idx, results = bm25search_command(args.query, args.limit)
+            count = 1
+            for doc_id, score in results:
+                doc = idx.docmap[doc_id]
+                print(f"{count}. ({doc['id']}) {doc['title']} - Score: {score:.2f}")
+                count += 1
         case _:
             parser.print_help()
 
---
+++ cli/semantic_search_cli.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from lib.semantic_search import embed_text, verify_model
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    verify_parser = subparsers.add_parser("verify", help="Verify model")
+    embed_parser = subparsers.add_parser("embed_text", help="Verify model")
+    embed_parser.add_argument("text", type=str, help="Text to embed")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "verify":
+            verify_model()
+        case "embed_text":
+            embed_text(args.text)
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
